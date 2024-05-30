8cf9e643428404ae4d7067ab191c67cd192d257e
kmzr9b67 <kmzr9b67@gmail.com>
2024-05-30 18:27:58 +0200

    Changing database to Pandas Dataframe. Adding Data.csv

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
-
+from json import loads, dumps
+import pandas as pd
 import requests
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pandas import read_csv
 from app.creds import API_KEY
 from app.database import Base, engine, l_session
 from app.models import InfoWaw
@@ -10,6 +12,15 @@ URL_AIR_QL = (f'https://api.um.warszawa.pl/api/action/air_sensors_get/?apikey={A
 
 Base.metadata.create_all(engine)
 app = FastAPI()
+
+def get_data(district):
+    data = read_csv('app/Data.csv')
+    data.index = data['District']
+    try:
+        return data.loc[district.title()]
+    except KeyError:
+        raise HTTPException(404, f'There is no information about such a district.'
+                             f'Please select from the list{data['District'].tolist()}')
 
 
 def request_api(url):
@@ -21,27 +32,23 @@ def get_air_quality(url, district):
     result = request_api(url)
     return [{'street': result[i]["address"]["street"],
              'air quality': result[i]["ijp"]["name"]} for i in range(0, len(result))
-            if result[i]["address"]["district"] == district]
+            if result[i]["address"]["district"] == district.title()]
 
 
 @app.get("/")
 async def introduction():
-    return 'Put the district name after the slash (e.g. /Żoliborz).'
+    return 'Put the district name after the slash (e.g./Żoliborz).'
 
 
 @app.get("/{district}")
 async def get_data_db_info(district: str):
-    air_quality = get_air_quality(URL_AIR_QL, district)
     try:
-        dane = l_session.query(InfoWaw).filter(InfoWaw.District == district.title()).all()[0]
-        return {district.title():
-                    {'Crimes': dane.Crimes,
-                     'Density of population': dane.Density,
-                     'Green spaces per person': dane.Green_space},
-                'air quality': air_quality}
+        air_quality = get_air_quality(URL_AIR_QL, district)
+        dane = get_data(district)
+        return dane, air_quality
     except IndexError:
         return 'No data available for this district. The district is not on the list.'
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", reload=True)
+    uvicorn.run("app.main:app", reload=True)
