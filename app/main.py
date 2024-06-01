038f6430e4c6a370cf1c52dad16a1713c1dbed0f
kmzr9b67 <kmzr9b67@gmail.com>
2024-06-01 22:51:13 +0200

    Deleting Air Quality WAW-API.

--- app/main.py
+++ app/main.py
@@ -4,35 +4,20 @@ import requests
 import uvicorn
 from fastapi import FastAPI, HTTPException
 from pandas import read_csv
-from app.creds import API_KEY
 from app.database import Base, engine, l_session
 from app.models import InfoWaw
 
-URL_AIR_QL = (f'https://api.um.warszawa.pl/api/action/air_sensors_get/?apikey={API_KEY}')
-
-Base.metadata.create_all(engine)
+# Base.metadata.create_all(engine)
 app = FastAPI()
+# def request_api(url):
+#     response = requests.get(url)
+#     return response.json()['result']
 
-def get_data(district):
-    data = read_csv('app/Data.csv')
-    data.index = data['District']
-    try:
-        return data.loc[district.title()]
-    except KeyError:
-        raise HTTPException(404, f'There is no information about such a district.'
-                             f'Please select from the list{data['District'].tolist()}')
-
-
-def request_api(url):
-    response = requests.get(url)
-    return response.json()['result']
-
-
-def get_air_quality(url, district):
-    result = request_api(url)
-    return [{'street': result[i]["address"]["street"],
-             'air quality': result[i]["ijp"]["name"]} for i in range(0, len(result))
-            if result[i]["address"]["district"] == district.title()]
+# def get_air_quality(url, district):
+#     result = request_api(url)
+#     return [{'street': result[i]["address"]["street"],
+#              'air quality': result[i]["ijp"]["name"]} for i in range(0, len(result))
+#             if result[i]["address"]["district"] == district.title()]
 
 
 @app.get("/")
@@ -42,13 +27,15 @@ async def introduction():
 
 @app.get("/{district}")
 async def get_data_db_info(district: str):
+    data = pd.read_csv('app/Data.csv')
+    data.index = data['District_name']
     try:
-        air_quality = get_air_quality(URL_AIR_QL, district)
-        dane = get_data(district)
-        return dane, air_quality
-    except IndexError:
-        return 'No data available for this district. The district is not on the list.'
+        return data.loc[district.title()].to_dict()
+    except KeyError:
+        raise HTTPException(404, f'There is no information about such a district.'
+                                 f'Please select from the list{data['District_name'].tolist()}')
 
 
 if __name__ == "__main__":
     uvicorn.run("app.main:app", reload=True)
+
